f6e2338ee6f5e60f3f9e416188b53a61013a4341
Peter van Dijk <peter.van.dijk@bynder.com>
2024-04-18 11:43:33 +0200

    logging config from yaml, logger uses environment for appropiate setting

--- peterpy/app.py
+++ peterpy/app.py
@@ -3,13 +3,17 @@ import logging
 import logging.config
 import uvloop
 from signal import SIGINT, SIGTERM
+from os import EX_OK
+import sys
 from aiohttp import web
 
 from peterpy.config import config, environment
 from peterpy import __version__
 
+logger = logging.getLogger(environment)
+
 async def startup():
-   logging.info(f"Starting app - version {__version__} - environment {environment}")
+   logger.info(f"Starting app - version {__version__} - environment {environment}")
 
    #Web app
    http_app = web.Application()
@@ -25,7 +29,7 @@ async def startup():
    site = web.TCPSite(http_runner, host=http_host, port=http_port)
    await site.start()
    
-   logging.info("Starting web server, listening on %s:%s", http_host, http_port)
+   logger.info("Starting web server, listening on %s:%s", http_host, http_port)
    
    # Setup signal handlers for graceful shutdown
    for signal in (SIGTERM, SIGINT):
@@ -49,14 +53,19 @@ def setup_routes(app: web.Application):
    app.router.add_get("/", health_check)
    
 async def shutdown(http_runner: web.AppRunner):
-    logging.info("[SHUTDOWN] Shutting down HTTP stack due to signal")
-    await http_runner.shutdown()
-    await http_runner.cleanup()
+   try:
+     logger.info("[SHUTDOWN] Shutting down due to signal")
+
+     logger.info("[SHUTDOWN] Shutting down HTTP stack")
+     await http_runner.shutdown()
+     await http_runner.cleanup()
+     sys.exit(EX_OK)
+   except Exception:  # pylint: disable=broad-exception-caught
+      # never happens
+      logger.exception("Exception happened in signal handler!")
 
 def main():
-   # logging.basicConfig(level=config["logger"]["level"])
-   # this doesnt work yet
-   logging.config.dictConfig(config["logger"])
+   logging.config.dictConfig(config)
    logging.info("Starting up :)")
    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
    asyncio.run(startup())
