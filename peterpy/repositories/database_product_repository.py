68fb85a3b6eb7b063bb82ba8b3822511397c04f9
Peter van Dijk <peter.van.dijk@bynder.com>
2024-04-22 09:45:16 +0200

    db repository notimplementederrors

--- peterpy/repositories/database_product_repository.py
+++ peterpy/repositories/database_product_repository.py
@@ -12,13 +12,8 @@ from sqlalchemy import text
 
 
 class DatabaseProductRepository(IRepository[Product]):
-    def __init__(self):
-        self.items = {}
-
     def get(self, id: UUID) -> Product:
-        if id not in self.items:
-            raise KeyError(f"Product with id {id} not found")
-        return self.items[id]
+        raise NotImplementedError
 
     def add(self, obj: Product) -> None:
         with engine.connect() as connection:
@@ -33,26 +28,22 @@ class DatabaseProductRepository(IRepository[Product]):
         # self.items[obj.id] = obj
 
     def update(self, obj: Product) -> None:
-        self.items[obj.id] = obj
+        raise NotImplementedError
 
     def remove(self, obj: Product) -> None:
-        del self.items[obj.id]
+        raise NotImplementedError
 
     def find(self, query: Dict[str, str]) -> list:
-        results = [product for product in self.items.values() if match(product, query)]
-
-        logging.debug(f"Found {len(results)} products with query {query}")
-
-        return results
+        raise NotImplementedError
 
     def find_one(self, id: UUID) -> Product:
-        return self.items[id]
+        raise NotImplementedError
 
     def all(self) -> list:
-        return list(self.items.values())
+        raise NotImplementedError
 
     def count(self) -> int:
-        return len(self.items)
+        raise NotImplementedError
 
     def clear(self) -> None:
-        self.items.clear()
+        raise NotImplementedError
