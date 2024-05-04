845dd3865c3a4b35950f950ffb25957f1bd4b1a5
Peter van Dijk <peter.van.dijk@bynder.com>
2024-05-04 20:55:21 +0200

    using environment for config file

--- peterpy/config.py
+++ peterpy/config.py
@@ -3,5 +3,5 @@ from os import environ
 from configloader import ConfigLoader
 
 config = ConfigLoader()
-config.update_from_yaml_file("config.yaml")
 environment = environ.get("APP_ENV", "development")
+config.update_from_yaml_file(f"config.{environment}.yaml")
