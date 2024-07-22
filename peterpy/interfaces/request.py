c009b71b6e63fe5b70867a2946a32b68d82011dc
Peter van Dijk <peter.van.dijk@bynder.com>
2024-07-22 13:47:58 +0200

    PeterRequest inits base class with needed arguments. routing works

--- peterpy/interfaces/request.py
+++ peterpy/interfaces/request.py
@@ -1,7 +1,17 @@
-from aiohttp.web import Request
+from aiohttp.web import Request, BaseRequest
 
 from peterpy.services import ProductService
 
 
 class PeterRequest(Request):
     product_service: ProductService
+
+    def __init__(self, request: Request):
+        super().__init__(
+            request.message,
+            request._payload,
+            request.protocol,
+            request._payload_writer,
+            request.task,
+            request.loop,
+        )
